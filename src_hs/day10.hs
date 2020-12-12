7587db196c6be70194195dd838864ac2bd23c75b
Jacob Xu <jxu023@ucr.edu>
2020-12-12 12:37:00 -0800

    hs day10 slow + learnings

--- src_hs/day10.hs
+++ src_hs/day10.hs
@@ -1,32 +1,40 @@
-import Debug.Trace (trace)
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 import Data.Maybe (mapMaybe)
+import Debug.Trace (trace)
 
 type Set = Set.Set
 type Map = Map.Map
 
--- TODO how to simplify?
-findChain :: Int -> Set Int -> Maybe [Int]
-findChain cur bag = if null bag then Just [cur] else do
-    let useChild val = if Set.member val bag then Just (val, Set.delete val bag)
-                                             else Nothing
-    let children = mapMaybe (uncurry findChain) $ mapMaybe useChild [cur+1, cur+2, cur+3]
-    if null children then Nothing
-                     else Just (cur: head children)
+-- TODO memoize
+findChainWithCond :: (Int -> Set Int -> Bool) -> Int -> Set Int -> [[Int]]
+findChainWithCond cond = findChain
+    where findChain cur bag = if cond cur bag then [[cur]] else do
+              let useChild val = if Set.member val bag then Just (val, Set.delete val bag)
+                                                       else Nothing
+              child <- concatMap (uncurry findChain) $ mapMaybe useChild [cur+1, cur+2, cur+3]
+              return (cur:child)
 
 type Freq = Map Int Int
 addNum :: Int -> Freq -> Freq
 addNum x freq = Map.insert x (count + 1) freq
     where count = Map.findWithDefault 0 x freq
 
-solveP1 :: [Int] -> Maybe Int
+solveP1 :: [Int] -> [Int]
 solveP1 nums = do
     let numSet = Set.fromList nums
+    let findChain = findChainWithCond (\_ s -> null s)
     chain <- findChain 0 (Set.insert (Set.findMax numSet + 3) numSet)
     let diffs = zipWith (-) (tail chain) chain
     let freq = foldr addNum Map.empty diffs
-    return $ Map.findWithDefault 0 1 freq * Map.findWithDefault 0 3 freq
+    let getFreq key = Map.findWithDefault 0 key freq
+    return $ getFreq 1 * getFreq 3
+
+solveP2 :: [Int] -> Int
+solveP2 nums =
+    let numSet = Set.fromList nums
+        maxNum = Set.findMax numSet
+        in length $ findChainWithCond (\val _ -> val == maxNum) 0 (Set.insert (maxNum + 3) numSet)
 
 main :: IO ()
 main = do
@@ -38,6 +46,9 @@ main = do
     batch <- getNums "../batch.input"
 
     print $ solveP1 example
-    print $ solveP1 batch
+    print . head $ solveP1 batch
+
+    print $ solveP2 example
+    print $ solveP2 batch
 
     print "bye"
