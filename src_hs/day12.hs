5bcd0d4f026ea5f03f1a8aa1b15b9471e3b82cd5
Jacob Xu <jxu023@ucr.edu>
2020-12-13 14:25:56 -0800

    hs day 12 p2

--- src_hs/day12.hs
+++ src_hs/day12.hs
@@ -7,31 +7,33 @@ addCoord = opCoord (+)
 multCoord d (a, b) = (d*a, d*b)
 
 --      North  East   South   West
-dirs = [(0,1), (1,0), (0,-1), (-1,0)]
-data Position = Position { pCoord :: Coord
-                         , pDir :: Int -- index into dirs
+data Position = Position { pShip :: Coord
+                         , pWayPt :: Coord -- index into dirs
                          } deriving (Show)
-initPos = Position (0, 0) 1
+initPos = Position (0, 0) (10, 1)
 
 dir cmd = case cmd of 'N' -> (0, 1)
                       'E' -> (1, 0)
                       'S' -> (0, -1)
                       'W' -> (-1, 0)
 
+turnRight (a, b) = (b, -a)
+turnLeft (b, a) = (-a, b)
+
 act :: Position -> (Char, Int) -> Position
-act pos (cmd, dist)
-    | cmd `elem` "NESW" = pos { pCoord = addCoord (pCoord pos) (multCoord dist (dir cmd)) }
-    | cmd == 'F' = pos { pCoord = addCoord (pCoord pos) (multCoord dist (dirs !! pDir pos)) }
-    | otherwise = pos { pDir = (pDir pos + (dist `div` 90) * sign) `mod` 4 }
-        where sign = case cmd of 'L' -> -1
-                                 'R' -> 1
-                                 c   -> error ("err:" ++ show c)
+act pos@(Position ship waypt) (cmd, dist)
+    | cmd `elem` "NESW" = pos { pWayPt = addCoord waypt (multCoord dist (dir cmd)) }
+    | cmd == 'F' = pos { pShip = addCoord ship (multCoord dist waypt) }
+    | otherwise = pos { pWayPt = last . take (1 + (dist `div` 90)) $ iterate turn waypt }
+          where turn | cmd == 'L' = turnLeft
+                     | cmd == 'R' = turnRight
+                     | otherwise = error ("unknown cmd" ++ show cmd)
 
 steerFerry :: String -> Position
 steerFerry contents =
     let acts = map parseAct $ lines contents
         parseAct (c:str) = (c, read str :: Int)
-        in foldl' (\pos cmd -> trace (show pos) act pos cmd) initPos acts
+        in foldl' (\a b -> trace (show $ act a b) act a b) initPos acts
 
 main :: IO ()
 main = do
@@ -40,7 +42,8 @@ main = do
     example <- readFile "../example.input"
     batch <- readFile "../batch.input"
 
-    print $ steerFerry example
-    print $ steerFerry batch
+    let val (Position (a, b) _) = abs a + abs b
+    print .val $ steerFerry example
+    print . val $ steerFerry batch
 
     print "bye"
