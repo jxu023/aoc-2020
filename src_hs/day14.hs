f6b57d9a69a5bb0a08e0800ca98eb7cf1fa53d92
Jacob Xu <jxu023@ucr.edu>
2020-12-20 14:29:53 -0800

    hs day 14 p2

--- src_hs/day14.hs
+++ src_hs/day14.hs
@@ -1,6 +1,6 @@
 import qualified Data.Map as Map
 import Data.Char (isDigit)
-import Data.Bits (setBit, (.|.), (.&.), complement)
+import Data.Bits (clearBit, setBit, (.|.), (.&.), complement)
 import Data.Foldable (foldl')
 import Data.List (isPrefixOf)
 import Debug.Trace (trace)
@@ -27,12 +27,12 @@ nonNumToSpace = map toSpace
                     | otherwise = ' '
 
 -- at location store value up to 2^36-1
-parseMem :: String -> (Int, Integer)
+parseMem :: String -> (Integer, Integer)
 parseMem line = (read num1, read num2)
     where [num1, num2] = words (nonNumToSpace line)
 
 data Memory = Memory { mMask :: Mask
-                     , mMem :: Map Int Integer
+                     , mMem :: Map Integer Integer
                      } deriving (Show)
 
 addLine :: Memory -> String -> Memory
@@ -45,6 +45,35 @@ solveP1 :: String -> Integer
 solveP1 contents = sum . Map.elems . mMem $ mem
     where mem = foldl' addLine (Memory (Mask 0 0) Map.empty) (lines contents)
 
+type FloatingMask = String
+
+parseFloatingMask :: String -> FloatingMask
+parseFloatingMask = unwords . drop 2 . words
+
+applyFloatingMask :: FloatingMask -> Integer -> [Integer]
+applyFloatingMask initMask initNum =
+    let vals = zip [35,34..0] initMask
+        addVal :: [Integer] -> (Integer, Char) -> [Integer]
+        addVal nums (i, c) | c == '1' = map (`setBit` fromIntegral i) nums
+                           | c == '0' = nums
+                           | c == 'X' = concat [[setBit n (fromIntegral i), clearBit n (fromIntegral i)] | n <- nums]
+                           | otherwise = error ("invalid mask char " ++ show c)
+        in foldl' addVal [initNum] vals
+
+data FloatingMem = FloatingMem { fmFloat :: FloatingMask
+                               , fmMem :: Map Integer Integer
+                               } deriving (Show)
+
+addFloatingLine :: FloatingMem -> String -> FloatingMem
+addFloatingLine (FloatingMem float mem) l | "mask" `isPrefixOf` l = FloatingMem (parseFloatingMask l) mem
+                                          | otherwise = FloatingMem float mem'
+    where mem' = foldr (`Map.insert` v) mem (applyFloatingMask float k)
+          (k, v) = parseMem l
+
+solveP2 :: String -> Integer
+solveP2 contents = sum . Map.elems . fmMem $ mem
+    where mem = foldl' addFloatingLine (FloatingMem "" Map.empty) (lines contents)
+
 main :: IO()
 main = do
     print "hi"
@@ -53,4 +82,7 @@ main = do
 
     print $ solveP1 example
     print $ solveP1 batch
+
+    print $ solveP2 example
+    print $ solveP2 batch
     print "bye"
