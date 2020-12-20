ff4b3f1f626eaad78ca83de5370db43ceb8e7cb9
Jacob Xu <jxu023@ucr.edu>
2020-12-20 11:52:35 -0800

    simplify Mask representation to 2 Integers

--- src_hs/day14.hs
+++ src_hs/day14.hs
@@ -1,23 +1,25 @@
 import qualified Data.Map as Map
 import Data.Char (isDigit)
-import Data.Bits (setBit, clearBit)
+import Data.Bits (setBit, (.|.), (.&.), complement)
 import Data.Foldable (foldl')
 import Data.List (isPrefixOf)
 import Debug.Trace (trace)
 
 type Map = Map.Map
--- TODO is there a better mask representation?
--- yes, just 2 nums, 1 for "or", another for "and complement"
--- we need 2 64 bit unsigned nums, but it doesn't really seem like we have one.
--- But, Integer (unlimited) should work just as well here.
-type Mask = [(Int, Bool)]
+
+data Mask = Mask { mSet :: Integer
+                 , mClear :: Integer
+                 } deriving (Show)
 
 -- for ea nonX char, i will get an index and a 0|1
 parseMask :: String -> Mask
-parseMask = foldl' f [] . zip [35,34..0] . unwords . drop 2 . words
-    where f res (i, c) | c == 'X' = res
-                       | c == '1' = (i, True):res
-                       | c == '0' = (i, False):res
+parseMask = foldl' f (Mask 0 0) . zip [35,34..0] . unwords . drop 2 . words
+    where f mask (i, c) | c == 'X' = mask
+                        | c == '1' = mask { mSet = setBit (mSet mask) i }
+                        | c == '0' = mask { mClear = setBit (mClear mask) i}
+
+applyMask :: Mask -> Integer -> Integer
+applyMask (Mask set clear) = (.&. complement clear) . (.|. set)
 
 nonNumToSpace :: String -> String
 nonNumToSpace = map toSpace
@@ -33,20 +35,15 @@ data Memory = Memory { mMask :: Mask
                      , mMem :: Map Int Integer
                      } deriving (Show)
 
-applyMask :: Integer -> Mask -> Integer
-applyMask = foldl' setVal
-    where setVal num (ind, set) | set = setBit num ind
-                                | otherwise = clearBit num ind
-
 addLine :: Memory -> String -> Memory
 addLine mem@(Memory mask m) l | "mask" `isPrefixOf` l = mem { mMask = parseMask l }
                               | otherwise = mem { mMem = Map.insert ind v m }
     where (ind, val) = parseMem l
-          v = applyMask val mask
+          v = applyMask mask val
 
 solveP1 :: String -> Integer
 solveP1 contents = sum . Map.elems . mMem $ mem
-    where mem = foldl' addLine (Memory [] Map.empty) (lines contents)
+    where mem = foldl' addLine (Memory (Mask 0 0) Map.empty) (lines contents)
 
 main :: IO()
 main = do
