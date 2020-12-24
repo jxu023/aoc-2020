235b56ada29eb3a6c8244c2a111b5fd655a43325
Jacob Xu <jxu023@ucr.edu>
2020-12-24 14:50:55 -0800

    interval map works! i should rename ranges to interval map..

--- src_hs/day16.hs
+++ src_hs/day16.hs
@@ -1,15 +1,17 @@
-import qualified Data.Map as Map
 import Debug.Trace (trace)
-import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Maybe (fromJust, mapMaybe)
+import qualified Data.Map as Map
 
 type Map = Map.Map
 
--- let's try writing out some unit tests while coding
-
-data RangeVal = Begin | End
+data RangeVal = Begin | End | Single
               deriving (Show, Eq)
+otherVal Begin = End
+otherVal End = Begin
+otherVal Single = Single
 type Ranges = Map Int RangeVal
 
+-- TODO try expressing this as a takeFirst and iterate () also
 -- deletes entries between indices in map (non-inclusive)
 deleteBetween :: Show a => Int -> Int -> Map Int a -> Map Int a
 deleteBetween l r ranges = if l == r then ranges else case Map.lookupGT l ranges of
@@ -17,15 +19,17 @@ deleteBetween l r ranges = if l == r then ranges else case Map.lookupGT l ranges
                              else deleteBetween x r (Map.delete x ranges)
     Nothing -> ranges
 
-endpt lookPast ind val ranges = fromMaybe ind $ do
-    (k, v) <- lookPast ind ranges
-    if v /= val && abs (k - ind) <= 1 then fst <$> lookPast k ranges
-                                      else Nothing
+-- how about initializing ranges with (-inf, End), (+inf, Begin)? would simplify the logic a bit
+endpt lookTowards lookPast ind val ranges = maybe ind f (lookTowards ind ranges)
+    where f (k, v) | v == otherVal val && abs (k - ind) <= 1 = fst . fromJust $ lookPast k ranges
+                   | otherwise = k 
 
+-- if single inRange .. then do nothing, otherwise add single .. easy ..
+-- if begin or end already has a single .. then just overwite it .
 addRange :: Int -> Int -> Ranges -> Ranges
 addRange begin end ranges =
-    let begin' = endpt Map.lookupLE begin Begin ranges
-        end' = endpt Map.lookupGE end End ranges
+    let begin' = endpt Map.lookupLE Map.lookupLT begin Begin ranges
+        end' = endpt Map.lookupGE Map.lookupGT end End ranges
         ranges' = deleteBetween begin' end' ranges
         in Map.insert end' End $ Map.insert begin' Begin ranges'
 
@@ -33,19 +37,22 @@ unionRanges :: Ranges -> Ranges -> Ranges
 unionRanges = Map.union
 
 inRanges :: Int -> Ranges -> Bool
-inRanges x ranges = undefined
+inRanges x ranges = xSingle || (snd <$> left) == Just Begin
+    where xSingle = Map.member x ranges && ranges Map.! x == Single
+          left = Map.lookupLE x ranges
 
 makeRanges :: [(Int, Int)] -> Ranges
 makeRanges = foldr (uncurry addRange) Map.empty
 
--- parsing (rules, your ticket, nearby tickets)
 type Rules = Map String Ranges
 type Ticket = [Int]
 
 parseContents :: String -> (Rules, Ticket, [Ticket])
-parseContents = undefined
+parseContents contents =
+    let [rs, t, ts] = splitOn [] (lines contents)
+        in undefined
 
-splitOn :: Char -> String -> [String]
+splitOn :: Eq a => a -> [a] -> [[a]]
 splitOn c = filter (not . null) . uncurry (:) . foldr f ([], [])
     where f d (cur, res) = if c == d then ([], cur:res)
                                      else (d:cur, res)
@@ -56,7 +63,6 @@ parseRules = map parseLine . lines
               where ws = words line
                     name = init $ head ws
                     ranges = map ((\[a,b] -> (read a, read b)) . splitOn '-')
-                                 . (\x -> trace (show x) x)
                                  . filter (/= "or")
                                  $ tail ws
 
@@ -79,13 +85,23 @@ expectEq a b test = do
 
 runUnitTests :: IO ()
 runUnitTests = do
+    expectEq (makeRanges [(1, 10)])
+             (Map.insert 1 Begin $ Map.insert 10 End Map.empty)
+             "makeRanges w/ single range"
     expectEq (deleteBetween 1 10 (makeRanges [(2, 3), (5, 7)]))
              (makeRanges [])
              "deleteBetween"
-    expectEq (endpt Map.lookupLE 5 Begin (makeRanges [(2, 3)]))
-             3
-             "endpt"
-    expectEq (addRange 31 60 (makeRanges [(1, 30), (50, 50), (52, 59), (60, 100)]))
+    expectEq (endpt Map.lookupLE Map.lookupLT 5 Begin (makeRanges [(2, 4)]))
+             2
+             "endpt diff by 1"
+    expectEq (endpt Map.lookupGE Map.lookupGT 2 End (makeRanges [(2, 5)]))
+             5
+             "endpt match on same"
+    expectEq (endpt Map.lookupGE Map.lookupGT 4 End (makeRanges [(2, 8)]))
+             8
+             "endpt match end on end"
+    -- TODO add test for singleNum ranges
+    expectEq (makeRanges [(31, 60), (1, 30), (52, 59), (60, 100)])
              (makeRanges [(1, 100)])
              "merging ranges"
     expectEq (inRanges 5 (makeRanges [(1, 3), (3, 10)]))
